b70861399298306d2653482ce019a65e898fb971
Gonu <mambomul@naver.com>
2024-05-17 18:30:41 +0900

    WIP: 프로젝트 페이지 우측 사이드바

---
+++ src/Projects/RightSideBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const SideBarContainer = styled.div`
+  width: 300px;
+  height: 80%;
+  background-color: #272727;
+`;
+
+const RightSideBar: React.FC = () => {
+  return <div>:React.FC</div>;
+};
+
+export default RightSideBar;
